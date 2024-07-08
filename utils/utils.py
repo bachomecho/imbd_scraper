2c8271cec2228c711418bb7947b7f465580c9f79
Ilia Mechkarov <64164772+bachomecho@users.noreply.github.com>
2024-07-08 15:31:07 +0300

    refactor: playlist utils included in utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,7 @@
 import inspect
 import sqlite3
+import requests
+from bs4 import BeautifulSoup
 
 def log(var):
     frame = inspect.currentframe()
@@ -19,3 +21,17 @@ def titles_currently_present(cur: sqlite3.Cursor) -> list[str]:
     res = cur.execute('SELECT title FROM movies')
     movie_titles = res.fetchall()
     return [title[0] for title in movie_titles]
+
+
+def get_playlist(url: str, user_agent: str):
+    res = requests.get(url, headers={'User-Agent': user_agent})
+    if res.status_code == 200:
+        return res.text
+    else:
+        raise Exception(f'Failed to fetch the page with following status code: {res.status_code}')
+
+
+def parse_playlist_for_ids(playlist_html: str):
+    soup = BeautifulSoup(playlist_html, 'html.parser')
+    links_with_ids = soup.find_all('a', class_ = 'ipc-title-link-wrapper', href=True)
+    return [link['href'].split('/')[2].lstrip('tt') for link in links_with_ids]
