e491ff7bc8beddd6587b7e1bcbe465c6c6601dbe
Ilia Mechkarov <64164772+bachomecho@users.noreply.github.com>
2024-07-28 12:23:23 +0300

    refactor: insert json into database

--- utils/handle_json.py
+++ utils/handle_json.py
@@ -14,41 +14,38 @@ def view_current_state(cur: sqlite3.Cursor, keys: list[str]):
         with open(f"db_dump_{str(datetime.today().date()).replace('-', '_')}.json", 'w') as out:
             json.dump(json_movies, out)
 
-def insert_json_into_db(con: sqlite3.Connection, cur: sqlite3.Cursor, json_file: Path):
+def insert_json_into_db(json_file: str):
+    database_path = 'REVISED_' + os.path.basename(json_file).strip('.json') + '.db'
+    con = sqlite3.connect(database_path)
+    if con: print(f'[log] Connection to {database_path} has been established.')
+    else: raise ConnectionRefusedError(f'Connection to {database_path} could not be established.')
+    cur = con.cursor()
+
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS movies (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        imdb_id TEXT UNIQUE,
+        title TEXT,
+        thumbnail_name TEXT,
+        video_id TEXT,
+        multi_part INTEGER,
+        duration INTEGER,
+        release_year INTEGER,
+        genre TEXT,
+        director TEXT,
+        plot TEXT
+    )
+    """
+    cur.execute(create_table_query)
     with open(json_file, 'r') as movies:
         data = json.load(movies)
-    print('data: ', type(data[0]))
     keys = list(dict(data[0]).keys())
-    log(keys)
-    cur.execute(f"PRAGMA table_info(movies);") # extracting column headers from table in db
-    columns_info = cur.fetchall()
-    db_column_headers = [info[1] for info in columns_info]
-    if 'id' in db_column_headers:
-        db_column_headers.remove('id')
-    log(db_column_headers)
-
-    new_cur = None
-    if db_column_headers != keys:
-        create_new_db = input('The column headers of the json file and database diverge. Do you want to create a new database with the columns that are present in the json? [y/n]')
-        if create_new_db == 'y':
-            new_path = input('Please provide a path for the new database: ')
-            is_file_name = False if '/' in new_path or '\\' in new_path else True
-            if new_path:
-                if is_file_name: new_con = sqlite3.connect(new_path)
-                else:
-                    if os.path.isabs(new_path): new_con = sqlite3.connect(new_path)
-                    else: print('[-] Invalid path. Either no path was provided or the given path is not absolute.')
-            new_cur = new_con.cursor()
     insert_query = f"""
         INSERT OR IGNORE INTO movies (
             {",".join(keys)}
         ) VALUES ({"".join([f':{name},' for name in keys]).strip(',')})
         """
 
-    if not new_cur:
-        cur.executemany(insert_query, data)
-    else:
-        new_cur.executemany(insert_query, data)
+    cur.executemany(insert_query, data)
     con.commit()
-    con.close()
-    print('[+] Json successfully migrated into db.')
+    del cur; del con
